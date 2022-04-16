2b9bf9246e523a601b74441d3efe4d7561345501
Brian Abbott <hi@brianabbott.com>
2022-04-16 09:34:26 -0400

    refactored js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
+const sideNav = document.querySelector('#side-nav')
+const body = document.querySelector('body')
+
 const openNav = () => {
-  document.querySelector('#side-nav').style.width = '250px'
+  sideNav.style.width = '250px'
 }
 
-document.querySelector('body').addEventListener('click', ev => {
-  const sideNav = document.querySelector('#side-nav')
+body.addEventListener('click', ev => {
   if ((sideNav.style.width === '250px') && (ev.target.id !== 'side-nav' && ev.target.className !== 'hamburger-menu' )) {
     sideNav.style.width = '0'
   }

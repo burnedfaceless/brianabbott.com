fd3f80fe2cf9ec7f8fae6ff5e06d0c1f401a22b3
Brian Abbott <hi@brianabbott.com>
2022-05-07 18:52:02 -0400

    refactored hamburger menu to use link instead of just image

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 const sideNav = document.querySelector('#side-nav')
 const body = document.querySelector('body')
+const hamburgerIcon = document.querySelector('.hamburger-menu')
 
 const openNav = () => {
   sideNav.style.width = '250px'
 }
 
-body.addEventListener('click', ev => {
-  if ((sideNav.style.width === '250px') && (ev.target.id !== 'side-nav' && ev.target.className !== 'hamburger-menu' )) {
+hamburgerIcon.addEventListener('click', e => {
+  e.preventDefault()
+  openNav()
+})
+
+body.addEventListener('click', e => {
+  if ((sideNav.style.width === '250px') && (e.target.id !== 'side-nav' && e.target.className !== 'hamburger' )) {
     sideNav.style.width = '0'
   }
 })
